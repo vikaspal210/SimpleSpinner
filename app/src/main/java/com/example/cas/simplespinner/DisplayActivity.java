0ac41223c70e18c86846fbd30bc6e2a8319545b3
Vikas Pal <pal.vikas210@gmail.com>
2018-09-03 02:08:25 +0530

    Added Bundle object to display selected spinner item

--- app/src/main/java/com/example/cas/simplespinner/DisplayActivity.java
+++ app/src/main/java/com/example/cas/simplespinner/DisplayActivity.java
@@ -2,6 +2,7 @@ package com.example.cas.simplespinner;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DisplayActivity extends Activity {
 
@@ -9,5 +10,11 @@ public class DisplayActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
+
+        TextView mTextView=(TextView)findViewById(R.id.display_tv);
+        //getting data from Bundle
+        Bundle mBundle=getIntent().getExtras();
+        String data=mBundle.get("data").toString();
+        mTextView.setText(data);
     }
 }
